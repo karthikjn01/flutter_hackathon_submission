be3da956bbd1507e0b9acc6579e1630a96205201
Karthik Nooli <karthik@supergoteam.com>
2020-06-27 14:30:14 +0100

    tiny fi

--- lib/Components/Card.dart
+++ lib/Components/Card.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/widgets.dart';
 
 class NoteCard extends StatelessWidget {
-
-  Note;
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Container();
   }
 }
